ea5388715677805d7d65e87fe408e2605c76a0bf
Douglas Kaip <dkaip@earthlink.net>
2019-09-02 18:22:14 -0600

    added vmaGetMemoryTypeProperties

---
+++ VMAJNIFunctions/vmaGetMemoryTypeProperties.cpp
@@ -0,0 +1,157 @@
+/*
+ * vmaGetMemoryTypeProperties.cpp
+ *
+ *  Created on: Sep 2, 2019
+ *      Author: Douglas Kaip
+ */
+
+#include "JVmaHelperFunctions.hh"
+#include "JVulkanHelperFunctions.hh"
+#include "com_CIMthetics_jvma_NativeProxies.h"
+#include "slf4j.hh"
+
+/*
+ * Class:     com_CIMthetics_jvma_NativeProxies
+ * Method:    vmaGetMemoryTypeProperties
+ * Signature: (Lcom/CIMthetics/jvma/Handles/VmaAllocator;ILjava/util/EnumSet;)V
+ */
+JNIEXPORT void JNICALL Java_com_CIMthetics_jvma_NativeProxies_vmaGetMemoryTypeProperties
+  (JNIEnv *env, jobject, jobject jVmaAllocator, jint memoryTypeIndex, jobject jVkMemoryPropertyFlags)
+{
+    struct VmaAllocator_T *vmaAllocator = (struct VmaAllocator_T *)jvma::getHandleValue(env, jVmaAllocator);
+    if (env->ExceptionOccurred())
+    {
+        LOGERROR(env, "%s", "Could not retrieve VmaAllocator handle.");
+        return;
+    }
+
+    if (jVkMemoryPropertyFlags == nullptr)
+    {
+        LOGERROR(env, "%s", "jVkMemoryPropertyFlags must nor be nullptr.");
+        return;
+    }
+
+    unsigned int flags = 0;
+    vmaGetMemoryTypeProperties(vmaAllocator, memoryTypeIndex, &flags);
+
+    jclass enumSetClass = env->GetObjectClass(jVkMemoryPropertyFlags);
+    if (env->ExceptionOccurred())
+    {
+        LOGERROR(env, "%s", "Could not get class for jVkMemoryPropertyFlags.");
+        return;
+    }
+
+    LOGERROR(env, "%s %d", "Flags", flags);
+
+    char const *enumClassString = "com/CIMthetics/jvulkan/VulkanCore/VK11/Enums/VkMemoryPropertyFlagBits";
+    char const *enumObjectString = "Lcom/CIMthetics/jvulkan/VulkanCore/VK11/Enums/VkMemoryPropertyFlagBits;";
+
+    jclass enumClass = env->FindClass(enumClassString);
+    if (env->ExceptionOccurred())
+    {
+        LOGERROR(env, "%s %s", "Could not get class for", enumClassString);
+        return;
+    }
+
+    jclass setClass = env->FindClass("java/util/Set");
+    jmethodID setAddMethod = env->GetMethodID(setClass, "add", "(Ljava/lang/Object;)Z");
+    if (env->ExceptionOccurred())
+    {
+        LOGERROR(env, "%s", "Could not find method id for add.");
+        return;
+    }
+
+    /*
+     * Make sure that flags does not have an unexpected value.  This would
+     * indicate that this code is out of sync with the LunarG Vulkan SDK.
+     */
+    if ((flags &
+         !(VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT |
+           VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+           VK_MEMORY_PROPERTY_HOST_COHERENT_BIT |
+           VK_MEMORY_PROPERTY_HOST_CACHED_BIT |
+           VK_MEMORY_PROPERTY_LAZILY_ALLOCATED_BIT |
+           VK_MEMORY_PROPERTY_PROTECTED_BIT)) != 0)
+    {
+        LOGERROR(env, "%s %d", "ERROR: Unhandled case for memoryPropertyFlags...value is", flags);
+        return;
+    }
+
+    if (flags & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)
+    {
+        jfieldID fieldId = env->GetStaticFieldID(enumClass, "VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT", enumObjectString);
+        jobject theEnum = env->GetStaticObjectField(enumClass, fieldId);
+
+        bool addResult = env->CallBooleanMethod(jVkMemoryPropertyFlags, setAddMethod, theEnum);
+        if (addResult == false)
+        {
+            LOGERROR(env, "%s", "ERROR: could not add VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT to EnumSet");
+            return;
+        }
+    }
+
+    if (flags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)
+    {
+        jfieldID fieldId = env->GetStaticFieldID(enumClass, "VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT", enumObjectString);
+        jobject theEnum = env->GetStaticObjectField(enumClass, fieldId);
+
+        bool addResult = env->CallBooleanMethod(jVkMemoryPropertyFlags, setAddMethod, theEnum);
+        if (addResult == false)
+        {
+            LOGERROR(env, "%s", "ERROR: could not add VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT to EnumSet");
+            return;
+        }
+    }
+
+    if (flags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)
+    {
+        jfieldID fieldId = env->GetStaticFieldID(enumClass, "VK_MEMORY_PROPERTY_HOST_COHERENT_BIT", enumObjectString);
+        jobject theEnum = env->GetStaticObjectField(enumClass, fieldId);
+
+        bool addResult = env->CallBooleanMethod(jVkMemoryPropertyFlags, setAddMethod, theEnum);
+        if (addResult == false)
+        {
+            LOGERROR(env, "%s", "ERROR: could not add VK_MEMORY_PROPERTY_HOST_COHERENT_BIT to EnumSet");
+            return;
+        }
+    }
+
+    if (flags & VK_MEMORY_PROPERTY_HOST_CACHED_BIT)
+    {
+        jfieldID fieldId = env->GetStaticFieldID(enumClass, "VK_MEMORY_PROPERTY_HOST_CACHED_BIT", enumObjectString);
+        jobject theEnum = env->GetStaticObjectField(enumClass, fieldId);
+
+        bool addResult = env->CallBooleanMethod(jVkMemoryPropertyFlags, setAddMethod, theEnum);
+        if (addResult == false)
+        {
+            LOGERROR(env, "%s", "ERROR: could not add VK_MEMORY_PROPERTY_HOST_CACHED_BIT to EnumSet");
+            return;
+        }
+    }
+
+    if (flags & VK_MEMORY_PROPERTY_LAZILY_ALLOCATED_BIT)
+    {
+        jfieldID fieldId = env->GetStaticFieldID(enumClass, "VK_MEMORY_PROPERTY_LAZILY_ALLOCATED_BIT", enumObjectString);
+        jobject theEnum = env->GetStaticObjectField(enumClass, fieldId);
+
+        bool addResult = env->CallBooleanMethod(jVkMemoryPropertyFlags, setAddMethod, theEnum);
+        if (addResult == false)
+        {
+            LOGERROR(env, "%s", "ERROR: could not add VK_MEMORY_PROPERTY_LAZILY_ALLOCATED_BIT to EnumSet");
+            return;
+        }
+    }
+
+    if (flags & VK_MEMORY_PROPERTY_PROTECTED_BIT)
+    {
+        jfieldID fieldId = env->GetStaticFieldID(enumClass, "VK_MEMORY_PROPERTY_PROTECTED_BIT", enumObjectString);
+        jobject theEnum = env->GetStaticObjectField(enumClass, fieldId);
+
+        bool addResult = env->CallBooleanMethod(jVkMemoryPropertyFlags, setAddMethod, theEnum);
+        if (addResult == false)
+        {
+            LOGERROR(env, "%s", "ERROR: could not add VK_MEMORY_PROPERTY_PROTECTED_BIT to EnumSet");
+            return;
+        }
+    }
+}
